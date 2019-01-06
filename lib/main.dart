dc2c18c23307fcafffaa1e7717e15d18f7a9469d
Romit <romitzunjarrao71@gmail.com>
2019-01-05 22:58:44 -0500

    basic app completed

--- lib/main.dart
+++ lib/main.dart
@@ -20,56 +20,137 @@ class Ui extends StatefulWidget {
 }
 
 class _Ui extends State<Ui> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Calculator"),
-      ),
-      body: new Column(
-        children: <Widget>[
-          button("1"),
-        ],
+  String output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String _output = "0";
+  String operand = "0";
+
+  buttonPressed(buttonText) {
+    if (buttonText == "Clear") {
+      _output = "0";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "/") {
+      this.operand = buttonText;
+      this.num1 = double.parse(output);
+      this._output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already has a .");
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "x") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget button(String buttonText) {
+    return new Expanded(
+      child: new OutlineButton(
+        padding: new EdgeInsets.all(24.0),
+        child: new Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () => buttonPressed(buttonText),
       ),
     );
   }
-}
-
-Widget button(String text) {
-  return new Column(
-    children: <Widget>[
-      new Row(
-        children: <Widget>[
-          new OutlineButton(
-            child: new Text("1"),
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Calculator"),
+        ),
+        body: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Container(
+                  alignment: Alignment.centerRight,
+                  padding: new EdgeInsets.symmetric(
+                    vertical: 48.0,
+                    horizontal: 10.0,
+                  ),
+                  child: new Text(
+                    output,
+                    style: new TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              new Expanded(child: new Divider()),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      button("7"),
+                      button("8"),
+                      button("9"),
+                      button("/"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      button("4"),
+                      button("5"),
+                      button("6"),
+                      button("x"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      button("1"),
+                      button("2"),
+                      button("3"),
+                      button("-"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      button("."),
+                      button("0"),
+                      button("00"),
+                      button("+"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      button("Clear"),
+                      button("="),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
-          new OutlineButton(
-            child: new Text("2"),
-          ),
-          new OutlineButton(
-            child: new Text("3"),
-          ),
-          new OutlineButton(
-            child: new Text("4"),
-          ),
-        ],
-      ),
-      new Row(
-        children: <Widget>[
-          new OutlineButton(
-            child: new Text("1"),
-          ),
-          new OutlineButton(
-            child: new Text("2"),
-          ),
-          new OutlineButton(
-            child: new Text("3"),
-          ),
-          new OutlineButton(
-            child: new Text("4"),
-          ),
-        ],
-      ),
-    ],
-  );
+        ),
+      );
+    }
+  
 }
